3d01d6e73b1e0ea31ed06cfeedaff43d00048913
davbergen <david.bergendorff@gmail.com>
2020-12-20 16:41:39 +0100

    add camera and change screen orientation to portrait

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -3,15 +3,18 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera(720, 1280);
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
@@ -20,6 +23,7 @@ public class Game extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
